e7e44206d7c7448d80b897dc5f62424a82e07def
Nordin Khalid <nokh8135@user.uu.se>
2024-12-09 18:28:56 +0100

    Initial

---
+++ blub.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char const *argv[])
+{
+    char* random_string = "whazaaaaa";
+    printf("%s", random_string);    
+    return 0;
+}
+
